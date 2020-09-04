b132aa6342fe1eab8430a2d00444f07157880987
ergerica <ergerica76@gmail.com>
2020-09-03 22:53:45 -0400

    game

---
+++ BullsEye/BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Erica Ross on 3/31/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class AboutAuthorViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func  close(){
+           dismiss(animated: true, completion: nil)
+    }
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BullsEye/BullsEye/AoutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AoutViewController.swift
+//  BullsEye
+//
+//  Created by Erica Ross on 3/31/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
